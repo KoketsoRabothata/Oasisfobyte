6a36dec21d6fd8e0f926943a965f554e5c92caa8
Koketso Rabothata <rabothatakoketso0902@gmail.com>
2024-03-04 18:17:23 +0200

    first commit

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ hamburger.onclick = function(){
     navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
 }
+
+const animate1 = (word) => {
+    word.classList.add("animate");
+ }
+ 
+    let  word = document.querySelectorAll(".word") 
+        
+        const animateAll = (animate) => {
+            setTimeout( () => {
+                animate1(word[0]);
+                setTimeout(() =>{
+                    animate1(word[1]);
+                    setTimeout(() =>{
+                        animate1(word[2])
+                    }, 1000)
+                }, 1000) 
+            }, 1000) 
+ }
+ 
+ animateAll(animate1) 
